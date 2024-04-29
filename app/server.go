6f28124a02efda5fd4adffc80d93adfd79b24ecb
Sostene Munezero Bagira <munezerobagira@gmail.com>
2024-04-29 13:14:18 +0200

    feat: handle session on on connection

--- app/server.go
+++ app/server.go
@@ -7,19 +7,28 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379", err.Error())
 		os.Exit(1)
 	}
-
 	fmt.Println("Redis server started on port 6379")
 	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection:", err)
 	}
+	handleClientConnection(conn)
+}
 
-	conn.Write([]byte("+PONG\r\n"))
+func handleClientConnection(conn net.Conn) {
+	for {
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading from connection: ", err.Error())
+		}
+		fmt.Println("Received data: ", buf[:n])
+		conn.Write([]byte("+PONG\r\n"))
+		fmt.Println("Received connection")
+	}
 }
