f60215abd9b9c8dd6440748f5eaca6e44406f119
Sostene Munezero Bagira <munezerobagira@gmail.com>
2024-04-29 10:43:31 +0200

    chore: implement sample pong

--- app/server.go
+++ app/server.go
@@ -8,18 +8,18 @@ import (
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Println("Failed to bind to port 6379", err.Error())
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	fmt.Println("Redis server started on port 6379")
+	conn, err := listener.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		fmt.Println("Error accepting connection:", err)
 	}
+
+	conn.Write([]byte("+PONG\r\n"))
 }
