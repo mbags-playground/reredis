e948e00eea144caa58dc72eb9e407ff2e76eab5b
Sostene Munezero Bagira <munezerobagira@gmail.com>
2024-04-29 14:24:14 +0200

    feat: multiple connection from one client

--- app/server.go
+++ app/server.go
@@ -13,22 +13,32 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Println("Redis server started on port 6379")
-	conn, err := listener.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection:", err)
+	defer listener.Close()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+		}
+		go handleClientConnection(conn)
 	}
-	handleClientConnection(conn)
 }
 
 func handleClientConnection(conn net.Conn) {
+	defer conn.Close()
+
+	fmt.Println("Processing connectin")
 	for {
 		buf := make([]byte, 1024)
 		n, err := conn.Read(buf)
 		if err != nil {
+			if err.Error() == "EOF" {
+				fmt.Println("Connection closed")
+				break
+			}
 			fmt.Println("Error reading from connection: ", err.Error())
 		}
 		fmt.Println("Received data: ", buf[:n])
 		conn.Write([]byte("+PONG\r\n"))
-		fmt.Println("Received connection")
+
 	}
 }
